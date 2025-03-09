766d1ac5cbbab1aa99904db8f9d71480d2d7cd29
Muhammed Anshif <Muhammadanshif0@gmail.com>
2025-03-10 00:03:26 +0530

    Removed client folder from Git tracking

--- client/src/hooks/roomHost.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-import React from 'react'
-
-export const roomHosting=async(postData)=>{
-    const response=await axios.post('900/host',postData)
-    return response.data
-}
