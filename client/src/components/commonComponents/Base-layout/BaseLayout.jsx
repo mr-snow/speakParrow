dadffad24d3efaa6926aeeb495b98b60ad984874
Muhammed Anshif <Muhammadanshif0@gmail.com>
2025-09-11 10:41:41 +0530

    c1

--- client/src/components/commonComponents/Base-layout/BaseLayout.jsx
+++ client/src/components/commonComponents/Base-layout/BaseLayout.jsx
@@ -46,7 +46,7 @@ function BaseLayout({ children }) {
             onClick={() => navigate('/test')}
           >
             {' '}
-            TestPage
+            DemoPage
           </div>
         </div>
         <div className="   flex items-center justify-center ">
